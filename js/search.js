41ea3f9a5fe30714fb27bf1da3ee0c5338ba3fbd
Neil Satterfield <neilsatt@gmail.com>
2017-04-12 19:46:01 -0600

    update search to look for entire caption title

--- js/search.js
+++ js/search.js
@@ -3,17 +3,19 @@
 
             let searchBox = document.querySelector("#search");
             // console.log(searchBox);
-            searchBox.addEventListener('keypress', checkCaptionTitle); // listen once a change happens in the input box
+            searchBox.addEventListener('keypress', checkCaption); // listen once a change happens in the input box
             
-            function checkCaptionTitle() {
+            function checkCaption() {
                 // loop through all of the pictures and their captions
                 console.log(searchBox.value);
                 $('.pic-container a').each(function() { 
-                    console.log($(this).attr('data-title')); // caption
-                if ($(this).attr('data-title').toLowerCase().indexOf(searchBox.value.toLowerCase()) == -1) { // -1 if value not found, hide pics
-                    $(this).css("display", "none");
+                    //console.log($(this).attr('data-title')); // caption
+                
+                // Do a search based and only show the picture if the user enters the complete caption title
+                if ( $(this).attr('data-title').toLowerCase() === searchBox.value.toLowerCase() ) { 
+                    $(this).show();  // Show pic if it exists
                 } else {
-                    $(this).show(); 
+                     $(this).css("display", "none"); // hide
                 }      
             });
            } // end of function
